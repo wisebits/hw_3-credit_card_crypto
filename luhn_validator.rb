256d3bbaad9f167308aeff4d5dfc0b26f260b5ec
Elvis Saravia <ellfae@gmail.com>
2016-03-07 14:26:23 +0800

    added more refactoring on Part A

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -6,13 +6,13 @@ module LuhnValidator
 	def validate_checksum
 		nums_a = number.to_s.chars.map(&:to_i)
 		sum = 0
-		nums_a.reverse.each.with_index do |num, i|
+		nums_a.reverse_each.with_index do |num, i|
 			if i.even?
-				sum+=num
+				sum += num
 			else
-				sum+=((2 * num).divmod(10).inject(:+))
+				sum += ((2 * num).divmod(10).inject(:+))
 			end
 		end
-		return sum%10==0 ? true : false
+		sum % 10 == 0 ? true : false
 	end
 end
