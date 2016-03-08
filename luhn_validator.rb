21f9ca8096a604f3544645a9b3da8b0a88289ded
omarsar <ellfae@gmail.com>
2016-03-08 22:07:58 +0800

    added faster implementation

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -3,9 +3,31 @@ module LuhnValidator
 	# arguments: none
 	# assumes: a local String called 'number' exists
 	# returns: true/false whether last digit is correct
+	
+	#Faster checksum
 	def validate_checksum
 		nums_a = number.to_s.chars.map(&:to_i)
 		sum = 0
+
+		nums_a.reverse_each.with_index do |num, i|
+			if i.even?
+				sum += num
+			else
+				if (num * 2) <= 9
+					sum += num * 2
+				else
+					sum += ((num * 2) / 10) + ((num * 2) % 10)
+				end
+			end
+		end
+		sum % 10 == 0 ? true : false
+	end
+
+	#Slower but more elegant
+	def elegant_validate_checksum
+		nums_a = number.to_s.chars.map(&:to_i)
+		sum = 0
+
 		nums_a.reverse_each.with_index do |num, i|
 			if i.even?
 				sum += num
