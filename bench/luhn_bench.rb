ce62318f8d3c259867f8d94baf89ba0f3ba49408
omarsar <ellfae@gmail.com>
2016-03-10 16:01:38 +0800

    added benchmark test for all

--- bench/luhn_bench.rb
+++ bench/luhn_bench.rb
@@ -8,25 +8,31 @@ Benchmark.bm(15) do |bench|
   card_numbers = n.times.map do
     card_number_length.times.map { Random.rand(10).to_s }.join
   end
-
-  10.times do 
-    bench.report('luhn_benchmark_nichole') do
+ 
+  bench.report('luhn_benchmark_nichole') do
+    10.times do
       card_numbers.each do |number|
         cc = CreditCard.new(number, nil, nil, nil)
         cc.validate_checksum_nichole
       end
     end
-    bench.report('luhn_benchmark_elvis') do
+  end
+
+  bench.report('luhn_benchmark_elvis') do
+    10.times do
       card_numbers.each do |number|
         cc = CreditCard.new(number, nil, nil, nil)
         cc.validate_checksum_elvis
       end
     end
-    bench.report('luhn_benchmark_adi') do
+  end
+
+  bench.report('luhn_benchmark_adi') do
+    10.times do
       card_numbers.each do |number|
         cc = CreditCard.new(number, nil, nil, nil)
         cc.validate_checksum_adi
-      end
     end
   end
+
 end
