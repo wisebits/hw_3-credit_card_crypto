1b7fc4d7f8873276134961d435b8e61c188eaf97
nweat <nikkiweat@gmail.com>
2016-03-13 20:58:12 +0800

    transposition cypher

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,15 +1,34 @@
 module DoubleTranspositionCipher
+
   def self.encrypt(document, key)
-    # TODO: FILL THIS IN!
+      # TODO: FILL THIS IN!
     ## Suggested steps for double transposition cipher
     # 1. find number of rows/cols such that matrix is almost square
     # 2. break plaintext into evenly sized blocks
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+    document = document.to_s.chars.map(&:to_s)
+
+    size = Math.sqrt(document.length).ceil
+
+    # test matrix
+    output = document.each_slice(size).to_a
+    output.each do |x| 
+      puts "#{x}" 
+    end
+
+
+    
+  
   end
+
+  
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
   end
 end
+
+document = 'attackatdawn'
+DoubleTranspositionCipher.encrypt(document, 3)
--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
+require_relative '../double_trans_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -33,16 +34,16 @@ describe 'Test card info encryption' do
       dec.must_equal @cc.to_s
     end
   end
-
+  # add tests for transposition
   describe 'Using Double transposition cipher' do
     it 'should encrypt card information' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
+      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
       enc.wont_equal @cc.to_s
     end
 
     it 'should decrypt text' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
+      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+      dec = DoubleTranspositionCipher.decrypt(enc, @key)
       dec.must_equal @cc.to_s
     end
   end
