153521f32a2098f05787d803b936e21f6d4d10f8
adi-botak- <adityautamawijaya@gmail.com>
2016-03-15 08:53:03 +0800

    Added Caesar and Permutation cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -4,9 +4,10 @@ module SubstitutionCipher
     # Arguments:
     #   document: String
     #   key: Fixnum (integer)
-    # Returns: String
+    # Returns: String 
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      key > 0 ? document.to_s.chars.map { |p| (p.ord + key).chr }.join : caesar_error
     end
 
     # Decrypts String document using integer key
@@ -16,6 +17,11 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      key > 0 ? document.to_s.chars.map { |c| (c.ord - key).chr }.join : caesar_error
+    end
+
+    def caesar_error
+    	"Key must be positive!"
     end
   end
 
@@ -27,6 +33,12 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      if key > 0
+      	permutation = (0..127).to_a.shuffle(random: Random.new(key))
+      	document.to_s.chars.map { |p| permutation[p.ord].chr }.join
+      else
+      	permutation_error
+      end
     end
 
     # Decrypts String document using integer key
@@ -36,6 +48,16 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      if key > 0
+      	permutation = (0..127).to_a.shuffle(random: Random.new(key))
+      	document.to_s.chars.map { |c| permutation.index(c.ord).chr }.join
+      else
+      	permutation_error
+      end
+    end
+
+    def permutation_error
+    	"Key must be positive!"
     end
   end
 end
