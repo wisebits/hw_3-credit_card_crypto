e5a216f6e85b90ccb2ee063d37eeccec047e7a6e
omarsar <ellfae@gmail.com>
2016-03-17 22:02:58 +0800

    added more metaprogramming goodness

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -3,6 +3,26 @@ require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
 require 'minitest/autorun'
 
+
+define_method :testing do |my_args|
+  describe 'Using #{my_args} cipher' do
+    it 'should encrypt card information' do
+      @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
+      @key = 3
+
+      enc = my_args.encrypt(@cc,@key)
+      dec = my_args.decrypt(enc,@key)
+      enc.wont_equal @cc.to_s
+      dec.must_equal @cc.to_s
+    end
+  end
+end
+
+testing(SubstitutionCipher::Caesar)
+testing(SubstitutionCipher::Permutation)
+testing(DoubleTranspositionCipher)
+
+=begin
 ciphers = ['Caesar', 'Permutation', 'Double Transposition']
 
 describe 'Test card info encryption' do
@@ -42,3 +62,5 @@ describe 'Test card info encryption' do
     end
   end
 end
+=end
+
