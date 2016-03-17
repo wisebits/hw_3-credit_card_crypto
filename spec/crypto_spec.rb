c3bbe138d6cffebc62b48a8b20a8c2c5223d9958
nweat <nikkiweat@gmail.com>
2016-03-17 18:57:10 +0800

    Addedmore crypto specs

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
-require_relative "../double_trans_cipher"
+require_relative '../double_trans_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -37,13 +37,14 @@ describe 'Test card info encryption' do
 
   # TODO: Add tests for double transposition and AES ciphers
   #       Can you DRY out the tests using metaprogramming? (see lecture slide)
-  describe "Using Double Transposition Cipher" do
-    it "should encrypt card information" do
+  describe 'Using Double Transposition Cipher' do
+    it 'should encrypt card information' do
       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+      enc.must_be_instance_of String
       enc.wont_equal @cc.to_s
     end
 
-    it "should decrypt text" do
+    it 'should decrypt text' do
       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
       dec = DoubleTranspositionCipher.decrypt(enc, @key)
       dec.must_equal @cc.to_s
