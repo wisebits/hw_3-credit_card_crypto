059d5414e3264d346cd71e4b8624e20536f0dd62
adi-botak- <adityautamawijaya@gmail.com>
2016-03-17 19:23:59 +0800

    Applied metaprogramming and Rubocop

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -3,51 +3,42 @@ require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
 require 'minitest/autorun'
 
+ciphers = ['Caesar', 'Permutation', 'Double Transposition']
+
 describe 'Test card info encryption' do
   before do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
     @key = 3
   end
 
-  describe 'Using Caesar cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-    end
+  ciphers.each do |name|
+    describe 'Using #{name} cipher' do
+      it 'should encrypt card information' do
+        case name
+        when 'Caesar'
+          enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
+        when 'Permutation'
+          enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
+        when 'Double Transposition'
+          enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+        end
+        enc.wont_equal @cc.to_s
+      end
 
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
-    end
-  end
-
-  describe 'Using Permutation cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-    end
-
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
-    end
-  end
-
-  # TODO: Add tests for double transposition and AES ciphers
-  #       Can you DRY out the tests using metaprogramming? (see lecture slide)
-  describe 'Using Double Transposition Cipher' do
-    it 'should encrypt card information' do
-      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
-      enc.must_be_instance_of String
-      enc.wont_equal @cc.to_s
-    end
-
-    it 'should decrypt text' do
-      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
-      dec = DoubleTranspositionCipher.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
+      it 'should decrypt text' do
+        case name
+        when 'Caesar'
+          enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
+          dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
+        when 'Permutation'
+          enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
+          dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
+        when 'Double Transposition'
+          enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+          dec = DoubleTranspositionCipher.decrypt(enc, @key)
+        end
+        dec.must_equal @cc.to_s
+      end
     end
   end
 end
