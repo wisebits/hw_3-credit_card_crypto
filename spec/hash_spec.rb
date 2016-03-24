8358312dae03eb1d075f41d48e21c8665eb103b3
adi-botak- <adityautamawijaya@gmail.com>
2016-03-24 19:44:58 +0800

    Deleted extra hash method

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -13,10 +13,6 @@ cards = card_details.map { |c| CreditCard.new(c[:num], c[:exp], c[:name], c[:net
 describe 'Test hashing requirements' do
   describe 'Test regular hashing' do
     describe 'Check hashes are consistently produced' do
-      cards.each do |cardinfo|
-        #hash = cardinfo.hash
-        #hash.wont_be_empty  --gives nilclass error
-      end
       # TODO: Check that each card produces the same hash if hashed repeatedly
       it 'should produce the same hash for identical cards' do
         hashes = []
